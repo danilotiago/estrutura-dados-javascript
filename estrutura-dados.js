057c283c9fb8a959ff2c96401a87334503f9e6a8
danilotiago <programacao.desenvolvimento@gmail.com>
2017-09-09 22:02:18 -0300

    criando array

---
+++ estrutura-dados.js
@@ -0,0 +1,28 @@
+function criacaoArrays()
+{
+    /**
+     * Tipo 1
+     *
+     * vetor = new Array(1,2,3,4);
+     * vetor = new Array("A","B","C","D");
+     */
+
+    /**
+     * Tipo 2
+     *
+     * vetor = Array(1,2,3,4);
+     * vetor = Array("A","B","C","D");
+     */
+
+    /**
+     * Tipo 3
+     *
+     * vetor = [1,2,3,4];
+     * vetor = ["A","B","C","D"];
+     */
+
+    // melhor forma
+    vetor = [1,2,3,4];
+
+    document.write(vetor);
+}
