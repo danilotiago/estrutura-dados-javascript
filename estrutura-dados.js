92d3809771a4f22b971a8d95533b73ac6cf0a4af
danilotiago <programacao.desenvolvimento@gmail.com>
2017-09-09 22:22:31 -0300

    correcao metodo altera separador

--- estrutura-dados.js
+++ estrutura-dados.js
@@ -57,7 +57,7 @@ function concatenaArrays(vetor1, vetor2)
     document.write("Novo vetor: " + vetor);
 }
 
-function alteraSeparadorArray(vetor1, vetor2)
+function alteraSeparadorArray(vetor1)
 {
     // troca o separador dos valores do array, o novo separador deve ser passado por string para join()
     vetor = vetor1.join(" - ");
